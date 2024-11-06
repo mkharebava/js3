0f33a0708ef8e19f6386cd3a5a9a552578f13afb
mkharebava <m_kharebava4@cu.edu.ge>
2024-11-07 00:39:01 +0400

    js2

---
+++ script.js
@@ -0,0 +1,16 @@
+let age = 14; 
+let ticketPrice="ticket price"; 
+
+if (age <0 || age>120) {
+    console.log(`either you're lying or you're living in 3024`);
+}
+ else if (age <= 12) {
+    console.log(`your ${ticketPrice} is $5`);
+} else if (age <= 17) {
+    console.log(`your ${ticketPrice} is $10`);
+}  else if (age <=65 ) {
+    console.log(`your ${ticketPrice} is $15`);
+}  else {
+    console.log(`your ${ticketPrice} is $8`);
+}
+/*areulad tu dawert metobebic dachirdeboda */
